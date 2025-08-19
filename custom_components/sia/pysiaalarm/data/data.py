0494225cecd946376a6308e7c9fe9c9d7f99f8cd
Alexandre-io <Alexandre-io@users.noreply.github.com>
2025-08-19 02:19:30 +0200

    Avoid blocking I/O by moving data from JSON to Python dict

--- custom_components/sia/pysiaalarm/data/data.py
+++ custom_components/sia/pysiaalarm/data/data.py
@@ -1,15 +1,12 @@
 """Data related utils for pysiaalarm."""
+
 from __future__ import annotations
 
-import json
 from dataclasses import dataclass
 
-import pkg_resources
-import asyncio
-
-FILE_SIA_CODES = "sia_codes.json"
-FILE_XDATA = "xdata.json"
-FILE_ADM_MAPPING = "adm_mapping.json"
+from .adm_mapping import ADM_MAPPING
+from .sia_codes import SIA_CODES
+from .xdata import XDATA
 
 
 @dataclass
@@ -34,29 +31,16 @@ class SIAXData:
     value: str | None = None
 
 
-def _load_data(file: str) -> dict:
-    """Load the one of the data json files."""
-    stream = pkg_resources.resource_stream(__name__, file)
-    return json.load(stream)
-
-async def _load_file(file:str) -> dict:
-    loop = asyncio.get_running_loop()
-    result = await loop.run_in_executor(None, _load_data, file)
-    return result
-
 def _load_sia_codes() -> dict[str, SIACode]:
     """Alias for loading sia codes file."""
-    task = asyncio.create_task(_load_file(FILE_SIA_CODES))
-    asyncio.get_running_loop().run_until_complete(task)
-    data = task.result()
-    return {key: SIACode(**value) for (key, value) in data.items()}
+    return {key: SIACode(**value) for (key, value) in SIA_CODES.items()}
+
 
 def _load_xdata() -> dict[str, SIAXData]:
     """Alias for loading xdata file."""
-    data = _load_data(FILE_XDATA)
-    return {key: SIAXData(**value) for (key, value) in data.items()}
+    return {key: SIAXData(**value) for (key, value) in XDATA.items()}
 
 
 def _load_adm_mapping() -> dict[str, dict[str, str]]:
     """Alias for loading adm mapping file."""
-    return _load_data(FILE_ADM_MAPPING)
+    return ADM_MAPPING
